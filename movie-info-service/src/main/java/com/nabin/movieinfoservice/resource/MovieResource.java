6f79e1124f4e34d93ebf777351b4a8b57b1bbe6b
thapanabin <thapanabincr7@outlook.com>
2019-10-22 20:34:02 -0500

    added controller class and configure port for each spring boot project

---
+++ movie-info-service/src/main/java/com/nabin/movieinfoservice/resource/MovieResource.java
@@ -0,0 +1,18 @@
+package com.nabin.movieinfoservice.resource;
+
+import com.nabin.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+
+        return new Movie(movieId,"TestName");
+
+    }
+}
