49b9b2373df8a577a497556bb6e31b3bc32f180a
thapanabin <thapanabincr7@outlook.com>
2019-10-24 14:09:59 -0500

    created eureka client

--- movie-info-service/src/main/java/com/nabin/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/nabin/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,11 @@ package com.nabin.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+//older version it is mandatory but now its not mandatory to enable to eureka client
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
