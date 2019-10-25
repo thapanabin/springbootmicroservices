a9604becd64ad52f1aa8660c93c0116ca8f13e8b
thapanabin <thapanabincr7@outlook.com>
2019-10-24 22:36:45 -0500

    added timeout to rest template

--- movie-catalog-service /src/main/java/com/nabin/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service /src/main/java/com/nabin/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,10 +1,12 @@
 package com.nabin.moviecatalogservice;
 
+import com.netflix.ribbon.proxy.annotation.Http;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,7 +18,10 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
+
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+	    return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	@Bean
